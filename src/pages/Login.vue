9f3a2759dc0d7f1697891c117e2413c4c36c90b0
unknown <destri.18425003@student.ubl.ac.id>
2020-03-26 16:07:02 +0700

    first commit

---
+++ src/pages/Login.vue
@@ -0,0 +1,71 @@
+<template>
+  <q-page class="bg-green-10">
+    <div class="row">
+      <q-card class="fixed-center col-md-4 col-xs-12 bg-green" flat>
+        <q-card-section>
+          <div class="text-h6 q-pb-md">
+            Halaman Login
+          </div>
+          <q-form
+            @submit="onSubmit"
+            @reset="onReset"
+            class="q-gutter-md"
+          >
+            <q-input
+              filled
+              v-model="username"
+              label="Username Anda"
+              lazy-rules
+              :rules="[ val => val && val.length > 0 || 'Tolong Ketik Username']"
+            />
+
+            <q-input
+              filled
+              type="password"
+              v-model="password"
+              label="Password Anda"
+              lazy-rules
+              :rules="[
+                val => val !== null && val.length > 0 || 'Password Tidak Boleh Kosong'
+              ]"
+            />
+            <div>
+              <q-btn label="Login" type="submit" color="primary"/>
+              <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+            </div>
+        </q-form>
+        </q-card-section>
+    </q-card>
+    </div>
+  </q-page>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      username: null,
+      password: null
+    }
+  },
+  methods: {
+    onSubmit () {
+      if (this.username === 'dewa' && this.password === '18421072') {
+        this.$q.notify({
+          type: 'positive',
+          message: 'Selamat Berhasil Login'
+        })
+        this.$router.push('/home')
+      } else {
+        this.$q.notify({
+          type: 'negative',
+          message: 'Gagal Login, Username/Password Salah'
+        })
+      }
+    },
+    onReset () {
+      this.username = null
+      this.password = null
+    }
+  }
+}
+</script>
